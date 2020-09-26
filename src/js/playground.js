478658ebdfd4791a67d4a3724b017c4380f08df9
Kundan Bhasin <kundanbhasin11062000@gmail.com>
2020-09-26 15:16:34 +0530

    added grid to ground

--- src/js/playground.js
+++ src/js/playground.js
@@ -12,11 +12,34 @@ let $lifeBox = document.querySelector('#playground-header .life');
 let $msgBox = document.querySelector('#playground-body .message');
 let $msgWrp = document.querySelector('#playground-body .message span');
 
+let grid = {};
+
+// ========================
+// grid
+
+grid.cellSize = 25;
+grid.cellMargin = 2;
+grid.maxCells = null;
+grid.margin = null;
+
+// ========================
+// setup
+
 let initGround = () => {
     canvas.width = +getComputedStyle(canvas).width.replace('px', '');
     canvas.height = +getComputedStyle(canvas).height.replace('px', '');
     canvas.style.width = 'unset';
     canvas.style.height = 'unset';
+
+    grid.maxCells = {
+        x: parseInt(canvas.width / grid.cellSize) - 1,
+        y: parseInt(canvas.height / grid.cellSize) - 1,
+    };
+
+    grid.margin = {
+        x: (canvas.width - grid.cellSize * (grid.maxCells.x + 1)) / 2,
+        y: (canvas.height - grid.cellSize * (grid.maxCells.y + 1)) / 2,
+    };
 
     setTimeout(() => {
         attach($msgBox);
